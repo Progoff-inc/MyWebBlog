ca2b6f62308d2853424d330bda8b6fd04645add1
IANomokonov <nomokonov.vana@yandex.ru>
2019-01-28 23:45:30 +0300

    #26: Закрывать меню

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,7 +4,8 @@ import { DeveloperService } from '../services/Developer.Service';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 import { LoadService } from '../services/load.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-nav',
@@ -15,12 +16,22 @@ export class NavComponent implements OnInit {
   user:Person;
   submitted = false;
   users:Person[];
+  showMenu = false;
   prevForm:FormGroup;
   modalRef2:BsModalRef;
   constructor(private ls:LoadService, private router:Router, private dv:DeveloperService, private fb:FormBuilder, private modalService: BsModalService) { }
-
+  closeMenu(){
+    this.showMenu=!this.showMenu;
+  }
   ngOnInit() {
-    
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      this.showMenu = false;
+      
+      window.scrollTo(0, 0)
+     });
     if(localStorage.getItem('user')){
       this.user=JSON.parse(localStorage.getItem('user'));
     }
